1ae0e430494c303a32d15e05ce9cbc7e1ea371ab
Lettergram <Lettergramm@gmail.com>
2014-07-22 15:11:05 -0500

    updated to readme

--- src/testgenerationform.h
+++ src/testgenerationform.h
@@ -2,6 +2,7 @@
 #define TESTGENERATIONFORM_H
 
 #include <QMainWindow>
+#include <iostream>
 
 namespace Ui {
 class testGenerationForm;
@@ -16,20 +17,22 @@ public:
     explicit testGenerationForm(QWidget *parent = 0);
     ~testGenerationForm();
 
+
+
 private slots:
 
-    void on_defaultSlider_valueChanged(int value);
+    void on_acceptButton_clicked();
+    void on_cencelButton_clicked();
+
+    void on_chromeSlider_valueChanged(int value);
 
     void on_firefoxSlider_valueChanged(int value);
 
-    void on_acceptButton_clicked();
-
-    void on_cencelButton_clicked();
+    std::string selectedOptions();
 
 private:
 
     Ui::testGenerationForm *ui;
-
 };
 
 #endif // TESTGENERATIONFORM_H
