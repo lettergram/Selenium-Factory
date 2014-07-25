fbdf990c42dc9fbfef8cbacd3081a95b47c3d99f
Lettergram <Lettergramm@gmail.com>
2014-07-25 10:48:11 -0500

    Made files variable, testName, and suiteName as well

--- scripts/generateTests.py
+++ scripts/generateTests.py
@@ -35,25 +35,33 @@ except:
 else:
 
       finalXML = '<?xml version="1.0" encoding="UTF-8" ?>\n<!DOCTYPE suite (View Source for full doctype...)>\n<suite thread-count="4" name="suite" parallel="tests" junit="false configfailurepolicy="skip" skipfailedinvocationcounts="false" data-provider-thread-count="10" group-by-instance="false"\n\tpreserve-order="true" allow-return-values="false">\n\t<!--  Test  -->\n'
+        
+testName = testArr[1][0]
+suiteName = testArr[1][1]
 
 # Start at range one so we don't get headers
-for i in range(1,len(testArr)):
+for i in range(2,len(testArr)):
 
         testn = '\t<test name="Test-'+str(i)+'" junit="false" skipfailedinvocationcounts="false" preserve-order="true" group-by-instances="false" allow-return-values="false">\n'
 
+
         cap = '\t\t<parameter name="cap" value="'+testArr[i][0]+'" />\n'
 
-        platform = '\t\t<parameter name="platform" value="'+testArr[1][1]+'" />\n'
+        platform = '\t\t<parameter name="platform" value="'+testArr[i][1]+'" />\n'
 
         version = '\t\t<parameter name="browser" value="'+testArr[i][2]+'" />\n'
 
         device ='\t\t<parameter name="device" value="'+testArr[i][3]+'" />\n'
         
         orientation ='\t\t<parameter name="ori" value="'+testArr[i][4]+'" />\n'
+        
+        cinput1 = '\t\t<parameter name="input1" value="'+testArr[i][5]+'" />\n'
+        cinput2 = '\t\t<parameter name="input2" value="'+testArr[i][6]+'" />\n'
+        cinput3 = '\t\t<parameter name="input3" value="'+testArr[i][7]+'" />\n'
 
         classes = '\t\t<classes>\n'
         
-        for j in range(5, len(testArr[i])):
+        for j in range(8, len(testArr[i])):
                 classes += '\t\t\t<class name="' + testArr[i][j] + '" />\n'
         
         classes += '\t\t</classes>'
@@ -68,7 +76,19 @@ for i in range(1,len(testArr)):
         finalXML += '\n\t</test>\n'
 
 finalXML += end
-filename = 'testXML/configTest' + '-' + str(randint(1,999999))
-filename += '-' + str(randint(1,999999)) + '.XML'
+filename = 'testXML/configTest'
+
+if testName != 'none':
+  
+        filename += '-' + testName
+else:
+        filename += '-' + str(randint(1,999999))
+
+if suiteName != 'none':
+        filename += '-' + suiteName
+else:
+        filename += '-' + str(randint(1,999999))
+
+filename += '.XML'
 f = open(filename, 'w')
 f.write(finalXML)
